1c86038934ebec84b598a24c36a98532fd5aaac3
HIMWLWDSJ <84259149+HIMWLWDSJ@users.noreply.github.com>
2023-06-22 20:25:00 +0800

    Update keybind.lua

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -1,17 +1,14 @@
 vim.g.mapleader= ' '
 vim.g.localleader= ' '
-local bind = vim.api.nvim_set_keymap
-local opt = {noremap = true,  silent = true}
-bind('n', '<leader>c', ':NvimTreeClose<CR>:NvimTreeOpen ~/.config/nvim<CR>', opt)   
-bind('n', '<C-l>', ':w<CR>:! m && ./out<CR>', opt)   
-bind('n', '<M-i>', 'ggo#include <', opt)   
-bind('n', '<S-tab>', ':NvimTreeFindFileToggle<CR>', opt)   
-bind('n', 'e', 'lbvec', opt)  
-bind('n', '；', 'bNext', opt)
-bind('n', 'q', ':q!<CR>', opt)  
-bind('n', 'Q', 'bdelete',opt);
+local bind = vim.keymap.set   
+bind('n', '<C-l>', ':w<CR>:! m <CR>', {})     
+bind('n', '<S-tab>', ':NvimTreeFindFileToggle<CR>', {})   
+bind('n', 'e', 'lbvec', {})  
+bind('n', ';', 'bNext', {})
+bind('n', 'q', ':q!<CR>', {})  
+bind('n', 'Q', 'bdelete',{})
 --telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>o', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+bind('n', '<leader>o', builtin.oldfiles, {})
+bind('n', '<leader>g', builtin.live_grep, {})
+bind('n', '<leader>b', builtin.buffers, {})
